92fcab20eadb192a9f7450edfa02e7f2314cf58f
Elijah Wilt <anon@anon.anon>
2018-12-19 12:36:04 -0500

    not updated python file?

--- Block.py
+++ Block.py
@@ -11,25 +11,23 @@ class Block(object):
     ##Blocks represented within 2D matrix space
     ##If 1 in spot [i][j], unit of block exists,
     ##otherwise if 0 in spot [i][j] then free space exists.
-    cardinality = -1
+    cardinality = 0
     matrix_rep = None
-    matrix_size  = (-1,-1)
-    arngmnt = None
 
     def __init__(self,matrix):
         matrix_rep = matrix
         cardinality = np.count_nonzero(matrix)
         matrix_size = matrix.shape
-        arngmnt = np.nonzero(matrix)
 
-    def rotate(self,dir=0,rots=1):
+    def is_valid(self,matrix):
+        xs,ys = np.nonzero(matrix)
+        
+
+    def rotate(self,dir=-1,rots=1):
         true_rots = rots % 4
-        if dir == 0:
-            new_mat = self.matrix_rep
-            for i in range(true_rots)
-            new_mat = np.rot90(self.matrix_rep)
+        new_mat = np.rot90(self.matrix_rep,true_rots)
 
     def reflect(self,axis=0):
-        if axis = 0:
+        if axis == 0:
             return np.flipud(self.matrix_rep)
         return np.fliplr(self.matrix_rep)
