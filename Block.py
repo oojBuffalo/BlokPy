8ec4e12612fb060b3b96c470849b494406d40953
Elijah Wilt <anon@anon.anon>
2019-02-12 14:30:21 -0500

    Added transformations method to block to make player less complicated and more readable.  Also redid blocktests with python unittest framework.

--- Block.py
+++ Block.py
@@ -28,7 +28,9 @@ class Block(object):
 
     def __init__(self,mat):
         self.__mat = mat
-        self.__dim,_ = mat.shape
+        r,c = mat.shape
+        assert r == c
+        self.__dim = r
         self.__card = np.count_nonzero(mat)
         assert self.is_valid()
 
@@ -49,34 +51,46 @@ class Block(object):
     #              matrix that contain a square from which to search outwards
     #       @count = int used to keep a total count of squares while recursing
     #OUTPUT: total count of connected squares with the block matrix
-    def num_connected(self,loc,count,visited):
+    def __num_connected(self,loc,count,visited):
         r,c = loc
         visited.append(loc)
-
-        #print(visited)
-
+        
         #check right
         if c+1 < self.__dim and self.__mat[r][c+1] and not (r,c+1) in visited:
-            count = self.num_connected((r,c+1),count+1,visited)
+            count = self.__num_connected((r,c+1),count+1,visited)
         #check down
         if r+1 < self.__dim and self.__mat[r+1][c] and not (r+1,c) in visited:
-            count = self.num_connected((r+1,c),count+1,visited)
+            count = self.__num_connected((r+1,c),count+1,visited)
         #check up
         if r-1 >= 0 and self.__mat[r-1][c] and not (r-1,c) in visited:
-            count = self.num_connected((r-1,c),count+1,visited)
+            count = self.__num_connected((r-1,c),count+1,visited)
         #check left
         if c-1 >= 0 and self.__mat[r][c-1] and not (r,c-1) in visited:
-            count = self.num_connected((r,c-1),count+1,visited)
+            count = self.__num_connected((r,c-1),count+1,visited)
         return count
 
-    def rotate(self,dir=-1,rots=1):
-        true_rots = rots % 4
-        return np.rot90(self.__mat,true_rots)
+    def rotate(self,rots=1):
+        return np.rot90(self.__mat,rots%4)
 
     def reflect(self,axis=0):
         if axis == 0:
             return np.flipud(self.__mat)
         return np.fliplr(self.__mat)
+
+        #player has move if one of their remaining blocks fits on the board
+        #i.e. there exists a block within the set of all transformations of all
+        #     remaining blocks such that the one of its transformations can be
+        #     placed on the board as a valid move.
+
+
+    #not mem nor time efficient - should be better algo that considers symmetry...
+    def transformations(self):
+        if self.__card == 1 : return [self.__mat]
+        cpy = self.__mat.copy()
+        rot = lambda m,n : np.rot90(m,n)
+        ref = np.fliplr(cpy)
+        ts = [cpy,rot(cpy,1),rot(cpy,2),rot(cpy,2),ref,rot(ref,1),rot(ref,2),rot(ref,3)]
+        return np.unique(ts)
 
     def get_mat(self):
         return self.__mat
@@ -86,3 +100,14 @@ class Block(object):
 
     def get_dim(self):
         return self.__dim
+
+    def to_string(self):
+        s = ''
+        dm = self.__dim
+        for i in range(dm*dm):
+            if i == 0: s += '-'*(dm*2+3)+'\n'
+            if i%dm == 0: s += '|'
+            s += str(self.__grid[int(i/dm)][i%dm])+'|'
+            if i%dm == dm-1: s += '|\n'
+            if i == dm*dm-1: s += '-'*(dm*2+3)+'\n'
+        return s
--- BlockTests.py
+++ BlockTests.py
@@ -1,12 +1,47 @@
 import Block
 import numpy as np
+import unittest
 
-def main():
-    cross_mat = np.vstack(([0,1,0],[1,1,1],[0,1,0]))
-    blok = Block.Block(cross_mat)
+class BlockTests(unittest.TestCase):
 
-    u_mat = np.vstack(([1,0,1],[1,1,1],[0,0,0]))
-    blok2 = Block.Block(u_mat)
+    def test_block_validity(self):
+        #mat not n-by-n
+        bad = np.vstack(([1,0,1],[1,1,1]))
+        self.assertRaises(AssertionError,Block.Block(bad))
+
+        #dim > card
+        bad = np.array([[1,0],[0,0]])
+        self.assertRaises(AssertionError,Block.Block(bad))
+
+        #card = 0
+        self.assertRaises(AssertionError,Block.Block(np.array([0])))
+
+        #card > 5
+        bad = np.ones(9).reshape(3,3)
+        self.assertRaises(AssertionError,Block.Block(bad))
+
+        #disjoint parts
+        bad = np.vstack(([1,1,1],[0,0,0],[1,0,0]))
+        self.assertRaises(AssertionError,Block.Block(bad))
+
+        bad = np.vstack(([1,0,0],[0,1,0],[0,0,1]))
+        self.assertRaises(AssertionError,Block.Block(bad))
+
+        bad = np.vstack(([1,0],[0,1]))
+        self.assertRaises(AssertionError,Block.Block(bad))
+
+        bad = np.vstack(([1,0,1],[0,0,0],[1,0,1]))
+        self.assertRaises(AssertionError,Block.Block(bad))
+
+        bad = np.vstack(([1,0,1],[0,1,0],[1,0,1]))
+        self.assertRaises(AssertionError,Block.Block(bad))
+
+        #good block
+        good = np.vstack(([1,0,1],[1,1,1],[0,0,0]))
+        self.assertIsInstance(Block.Block(good),Block)
+
+        #def test_transformations(self):
+
 
 if __name__ == '__main__':
-    main()
+    unittest.main()
